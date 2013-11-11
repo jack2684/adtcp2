74d7328e63ceca8b7a06241d8333187159cea2bb
Jack Junjie Guan <gjj2684@gmail.com>
2013-11-11 13:23:19 -0500

    compile the adtcp, seems ok

---
+++ ns-allinone-3.18/ns-3.18/build/ns3/adtcp-module.h
@@ -0,0 +1,12 @@
+
+#ifdef NS3_MODULE_COMPILATION
+# error "Do not include ns3 module aggregator headers from other modules; these are meant only for end user scripts."
+#endif
+
+#ifndef NS3_MODULE_ADTCP
+    
+
+// Module headers:
+#include "adtcp-helper.h"
+#include "adtcp.h"
+#endif
---
+++ ns-allinone-3.18/ns-3.18/build/ns3/adtcp.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef ADTCP_H
+#define ADTCP_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* ADTCP_H */
+
